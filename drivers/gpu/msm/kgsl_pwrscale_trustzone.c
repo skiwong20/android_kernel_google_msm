313791f9e6ae108153ae272702692fc8dbae7e8a
BjÃ¶rn Hutmacher <bjorn.hutmacher@gmail.com>
2013-11-25 14:32:00 +0100

    KGSL: Revert Simple GPU governor
    Revert "Fix KGSL derp"
    
    This reverts commit 61a6fc10bce777ed0213f678b40ec479ebd1a418.
    
    Revert "KGSL: Simple Governor: so I kinda butchered in a good way of course the original code from faux, but the simplicity is still here. Added an history so that we can track the average do that the frequency scaling is much more proporcional to the actual GPU load rather than having is ramp up every now and then due to spurious load spike. Also remove two useless ifs."
    
    This reverts commit dae1f62265162a2991de21fcca484869a261e1f9.
    
    Revert "KGSL: Simple Governor: some styling changes, early returns instead of assigning a val and returning in the end of the function call and change the copyright/author from the middle of the file to the header."
    
    This reverts commit d89e891848247e0d06b2a2266951d68c0a726926.
    
    Revert "KGSL: Simple Governor: fix minor logic issue"
    
    This reverts commit 49b1f281f086d6ff1a4c898703647aafca336f0d.
    
    Revert "KGSL: simple governor: expose tunables to sysfs interface via module_param"
    
    This reverts commit 787fca783b4911be713d2c5982d63773b64c74ba.
    
    Revert "KGSL: make simple govenor a selectable option, co-exist with ondemand"
    
    This reverts commit 67477a88ffe2811a6f5c45c729e43f35f76e8ea5.
    
    Revert "KGSL: Add a simple GPU governor for Adreno xxx GPU series"
    
    This reverts commit bccfddea2798b6a406fa8f2523e8b7c620bbc829.
    
    Conflicts:
    	drivers/gpu/msm/Kconfig

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -8,9 +8,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * Modified by Paul Reioux (Faux123)
- * 2013-06-20: Added KGSL Simple GPU Governor
  *
  */
 
@@ -26,15 +23,8 @@
 #include "kgsl_pwrscale.h"
 #include "kgsl_device.h"
 
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-#include <linux/module.h>
-#endif
-
 #define TZ_GOVERNOR_PERFORMANCE 0
 #define TZ_GOVERNOR_ONDEMAND    1
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-#define TZ_GOVERNOR_SIMPLE	2
-#endif
 
 struct tz_priv {
 	int governor;
@@ -48,10 +38,6 @@ spinlock_t tz_lock;
  * per frame for 60fps content.
  */
 #define FLOOR			5000
-/* CEILING is 50msec, larger than any standard
- * frame length, but less than the idle timer.
- */
-#define CEILING			50000
 #define SWITCH_OFF		200
 #define SWITCH_OFF_RESET_TH	40
 #define SKIP_COUNTER		500
@@ -85,10 +71,6 @@ static ssize_t tz_governor_show(struct kgsl_device *device,
 
 	if (priv->governor == TZ_GOVERNOR_ONDEMAND)
 		ret = snprintf(buf, 10, "ondemand\n");
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-	else if (priv->governor == TZ_GOVERNOR_SIMPLE)
-		ret = snprintf(buf, 8, "simple\n");
-#endif
 	else
 		ret = snprintf(buf, 13, "performance\n");
 
@@ -112,15 +94,11 @@ static ssize_t tz_governor_store(struct kgsl_device *device,
 
 	if (!strncmp(str, "ondemand", 8))
 		priv->governor = TZ_GOVERNOR_ONDEMAND;
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-	else if (!strncmp(str, "simple", 6))
-		priv->governor = TZ_GOVERNOR_SIMPLE;
-#endif
 	else if (!strncmp(str, "performance", 11))
 		priv->governor = TZ_GOVERNOR_PERFORMANCE;
 
 	if (priv->governor == TZ_GOVERNOR_PERFORMANCE)
-		kgsl_pwrctrl_pwrlevel_change(device, pwr->thermal_pwrlevel);
+		kgsl_pwrctrl_pwrlevel_change(device, pwr->max_pwrlevel);
 
 	mutex_unlock(&device->mutex);
 	return count;
@@ -141,60 +119,10 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
 	struct tz_priv *priv = pwrscale->priv;
 	if (device->state != KGSL_STATE_NAP &&
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-		(priv->governor == TZ_GOVERNOR_ONDEMAND ||
-		 priv->governor == TZ_GOVERNOR_SIMPLE))
-#else
 		priv->governor == TZ_GOVERNOR_ONDEMAND)
-#endif
 		kgsl_pwrctrl_pwrlevel_change(device,
 					device->pwrctrl.default_pwrlevel);
 }
-
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-#define HISTORY_SIZE 10
-
-static int ramp_up_threshold = 5500;
-module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
-
-static unsigned int history[HISTORY_SIZE] = {0};
-static unsigned int counter = 0;
-
-static int simple_governor(struct kgsl_device *device, int idle_stat)
-{
-	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
-	int i;
-	unsigned int total = 0;
-
-	history[counter] = idle_stat;
-
-	for (i = 0; i < HISTORY_SIZE; i++)
-		total += history[i];
-
-	total = total/HISTORY_SIZE;
-
-	if (++counter == 10)
-		counter = 0;
-
-	/* it's currently busy */
-	if (total < ramp_up_threshold) 
-	{
-		if ((pwr->active_pwrlevel > 0) &&
-			(pwr->active_pwrlevel <= (pwr->num_pwrlevels - 1)))
-			/* bump up to next pwrlevel */
-			return -1; 
-	} 
-	/* idle case */
-	else 
-	{
-		if ((pwr->active_pwrlevel >= 0) &&
-			(pwr->active_pwrlevel < (pwr->num_pwrlevels - 1)))
-			return 1; 	 
-	}
-
-	return 0;
-}
-#endif
 
 static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
@@ -235,31 +163,14 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 		priv->no_switch_cnt = 0;
 	}
 
-	/* If there is an extended block of busy processing,
-	 * increase frequency.  Otherwise run the normal algorithm.
-	 */
-	if (priv->bin.busy_time > CEILING) {
-		val = -1;
-	} else {
-		idle = priv->bin.total_time - priv->bin.busy_time;
-		idle = (idle > 0) ? idle : 0;
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-		if (priv->governor == TZ_GOVERNOR_SIMPLE)
-			val = simple_governor(device, idle);
-		else
-			val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
-#else
-		val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
-#endif
-	}
+	idle = priv->bin.total_time - priv->bin.busy_time;
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
-	if (val) {
+	idle = (idle > 0) ? idle : 0;
+	val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
+	if (val)
 		kgsl_pwrctrl_pwrlevel_change(device,
 					     pwr->active_pwrlevel + val);
-		//pr_info("TZ idle stat: %d, TZ PL: %d, TZ out: %d\n",
-		//		idle, pwr->active_pwrlevel, val);
-	}
 }
 
 static void tz_busy(struct kgsl_device *device,
@@ -279,25 +190,27 @@ static void tz_sleep(struct kgsl_device *device,
 	priv->bin.busy_time = 0;
 }
 
+#ifdef CONFIG_MSM_SCM
 static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
 	struct tz_priv *priv;
-
-	/* Trustzone is only valid for some SOCs */
-	if (!(cpu_is_msm8x60() || cpu_is_msm8960() || cpu_is_apq8064() ||
-		cpu_is_msm8930() || cpu_is_msm8930aa() || cpu_is_msm8627()))
-		return -EINVAL;
 
 	priv = pwrscale->priv = kzalloc(sizeof(struct tz_priv), GFP_KERNEL);
 	if (pwrscale->priv == NULL)
 		return -ENOMEM;
 
-	priv->governor = TZ_GOVERNOR_SIMPLE;
+	priv->governor = TZ_GOVERNOR_ONDEMAND;
 	spin_lock_init(&tz_lock);
 	kgsl_pwrscale_policy_add_files(device, pwrscale, &tz_attr_group);
 
 	return 0;
 }
+#else
+static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
+{
+	return -EINVAL;
+}
+#endif /* CONFIG_MSM_SCM */
 
 static void tz_close(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
